c9adb698b27c6ca7c7a473246d402923d43d015e
qwerty123253 <lev_noskov33@mail.ru>
2026-01-14 18:35:26 +0300

    BOT is here
    version 0. 2

--- pinpong.py
+++ pinpong.py
@@ -4,7 +4,7 @@ win = display.set_mode((800, 500))
 clock = time.Clock()
 class Player():
     def __init__(self, x, y, speed):
-        self.hitbox = Rect(x, y, 30, 150)
+        self.hitbox = Rect(x, y, 20, 150)
         self.speed = speed
 
     def move(self):
@@ -17,11 +17,20 @@ class Player():
             self.hitbox.bottom = 500
         if self.hitbox.top < 0:     
             self.hitbox.top = 0
+        if self.hitbox.colliderect(ball1.hitbox):
+            ball1.speed_x = ball1.speed
+
+    def AI(self):
+        self.hitbox.y = ball1.hitbox.y
+        if self.hitbox.colliderect(ball1.hitbox):
+            ball1.speed_x = -ball1.speed
+
+
 
 
 class Ball():
         def __init__(self, x, y, speed):
-            self.hitbox = Rect(x, y, 40, 40)
+            self.hitbox = Rect(x, y, 25, 25)
             self.speed = speed
             self.speed_x = speed
             self.speed_y = speed
@@ -34,13 +43,17 @@ class Ball():
                 self.speed_y = self.speed
             if self.hitbox.bottom > 500:
                 self.speed_y = -self.speed
-            if self.hitbox.left < 0:
-                self.speed_x = self.speed
-            if self.hitbox.right > 800:
-                self.speed_x = -self.speed
+            #if self.hitbox.left < 0:
+                #self.speed_x = self.speed
+            #if self.hitbox.right > 800:
+               #self.speed_x = -self.speed
+
+        
+
 
 player1 = Player(50, 240, 1)
-ball1 = Ball(45, 45, 1)
+ball1 = Ball(10, 10, 2)
+AI = Player(700, 240, 1)
 
 while True:
     win.fill((0, 0, 0))
@@ -50,6 +63,8 @@ while True:
             exit()
     player1.move()
     draw.rect(win, (255, 0, 0), player1.hitbox)
+    draw.rect(win, (0, 0, 255), AI.hitbox)
+    AI.AI()
     ball1.move()
     draw.rect(win, (0, 255, 0), ball1.hitbox)
     display.update()
