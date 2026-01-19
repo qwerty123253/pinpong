a8a8942f2da5bd62718a14bdcc1399e993bdb9a2
qwerty123253 <lev_noskov33@mail.ru>
2026-01-19 17:13:00 +0300

    i have added GAMEPLAY
    version 0.3

--- pinpong.py
+++ pinpong.py
@@ -1,4 +1,6 @@
 from pygame import *
+from random import *
+import sys
 win = display.set_mode((800, 500))
 
 clock = time.Clock()
@@ -20,17 +22,40 @@ class Player():
         if self.hitbox.colliderect(ball1.hitbox):
             ball1.speed_x = ball1.speed
 
+
     def AI(self):
-        self.hitbox.y = ball1.hitbox.y
+                # Добавляем небольшую случайную ошибку
+        target_y = ball1.hitbox.centery + randint(-120, 120)
+        
+        # Двигаемся к цели
+        if self.hitbox.centery < target_y:
+            self.hitbox.y += self.speed
+        elif self.hitbox.centery > target_y:
+            self.hitbox.y -= self.speed
+
+        # Ограничиваем движение в пределах экрана
+        if self.hitbox.top < 0:
+            self.hitbox.top = 0
+        if self.hitbox.bottom > 500:
+            self.hitbox.bottom = 500
+
+        # Отбиваем мяч
         if self.hitbox.colliderect(ball1.hitbox):
             ball1.speed_x = -ball1.speed
+            #self.hitbox.y = ball1.hitbox.y
+            #if self.hitbox.colliderect(ball1.hitbox):
+                #ball1.speed_x = -ball1.speed
+
+        
+
+        
 
 
 
 
 class Ball():
         def __init__(self, x, y, speed):
-            self.hitbox = Rect(x, y, 25, 25)
+            self.hitbox = Rect(x, y, 30, 30)
             self.speed = speed
             self.speed_x = speed
             self.speed_y = speed
@@ -43,17 +68,28 @@ class Ball():
                 self.speed_y = self.speed
             if self.hitbox.bottom > 500:
                 self.speed_y = -self.speed
-            #if self.hitbox.left < 0:
-                #self.speed_x = self.speed
-            #if self.hitbox.right > 800:
-               #self.speed_x = -self.speed
+            if self.hitbox.left < 0:
+                self.speed_x = self.speed
+                player1.hitbox.height -= 10
+                player1.speed += 0.2
+            if self.hitbox.right > 800:
+               self.speed_x = -self.speed
+               AI.hitbox.height += 10
+
+
 
         
 
 
-player1 = Player(50, 240, 1)
+
+        
+
+        
+
+
+player1 = Player(50, 240, 2)
 ball1 = Ball(10, 10, 2)
-AI = Player(700, 240, 1)
+AI = Player(700, 240, 2)
 
 while True:
     win.fill((0, 0, 0))
@@ -67,6 +103,17 @@ while True:
     AI.AI()
     ball1.move()
     draw.rect(win, (0, 255, 0), ball1.hitbox)
+
+
+
+    if player1.hitbox.height == 10:
+            quit()
+            sys.exit()
+
+    if AI.hitbox.height == 230:
+            quit()
+            sys.exit()
+
     display.update()
     clock.tick(500)
 
